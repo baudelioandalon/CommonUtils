2bbeae12ba3c0f65753ede31ddaef5d10d00348b
Baudelio <baudelio_andalon@hotmail.com>
2022-04-03 13:08:00 -0500

    [REFACTOR] Back pressed
    * OnBackPressed function modified in CUBaseFragment.kt

--- commonutils/src/main/java/com/boreal/commonutils/base/CUBaseFragment.kt
+++ commonutils/src/main/java/com/boreal/commonutils/base/CUBaseFragment.kt
@@ -7,12 +7,8 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.airbnb.lottie.LottieAnimationView
 import com.boreal.commonutils.extensions.disableBackButton
-import kotlin.reflect.KClass
 
 abstract class CUBaseFragment<T : ViewDataBinding> :
     Fragment() {
@@ -29,7 +25,7 @@ abstract class CUBaseFragment<T : ViewDataBinding> :
      */
     abstract fun getLayout(): Int
     open fun initDependency(savedInstanceState: Bundle? = null) {}
-    open fun initObservers(){}
+    open fun initObservers() {}
     abstract fun initView()
 
     override fun onCreateView(
@@ -93,13 +89,17 @@ abstract class CUBaseFragment<T : ViewDataBinding> :
         )
     }
 
-    /**
-     * Acción que se ejecuta cuando se detecta que se preciona el boton de back de android,
-     * esta funcion es llamada desde base activity
-     */
-    fun onFragmentBackPressed(): Boolean {
-        listenerBackPress?.onBackPress()
-        return false
+//    /**
+//     * Acción que se ejecuta cuando se detecta que se preciona el boton de back de android,
+//     * esta funcion es llamada desde base activity
+//     */
+//    fun onFragmentBackPressed(): Boolean {
+//        listenerBackPress?.onBackPress()
+//        return false
+//    }
+
+    fun onFragmentBackPressed() {
+        requireActivity().onBackPressed()
     }
 
     /**
